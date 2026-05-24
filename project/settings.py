4d692890417bed15920d79cb18ccec0277e5cf50
JeffersonEstevo <jefferson.estevo@gmail.com>
2026-05-24 15:36:47 -0300

    Criando o app de conctact no Django

--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'contact', # Adicionado após criação do app contact com $ python .\manage.py startapp contact
 ]
 
 MIDDLEWARE = [
